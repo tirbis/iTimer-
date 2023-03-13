046289a1e7f74ef90b03c880bb28c42912ce6bdc
tirbis <102221675+tirbis@users.noreply.github.com>
2023-03-13 12:24:11 +0300

    Add files via upload

---
+++ iTimer/iTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTimer
+//
+//  Created by Тирбулатов Ислам Асланович on 13/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
